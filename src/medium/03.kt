abf38a591994e960e1f7fd017621c6ba98f656c0
nerolink <3053318657@qq.com>
2018-10-25 22:24:44 +0800

    1111

--- src/medium/03.kt
+++ src/medium/03.kt
@@ -3,27 +3,28 @@ package medium
 import java.util.*
 import kotlin.collections.HashSet
 
-class Solution{
+class Solution {
     fun lengthOfLongestSubstring(s: String): Int {
+        if (s.isEmpty()) return 0
+        if (s.length == 1) return 1
         var queue = mutableListOf<Char>()
         val set = hashSetOf<Char>()
-        val temp=""
-        s.forEach { c->
-            if (set.contains(c)) {
-
-            } else {
+        var result = 0
+        s.forEach { c ->
+            if (!set.contains(c)) {
                 set.add(c)
+                queue.add(c)
+            } else {
+                if (queue.size > result) result = queue.size
+                while (queue[0] != c) set.remove(queue.removeAt(0))
+                queue.removeAt(0)
                 queue.add(c)
             }
         }
+        if (queue.size > result) {
+            result = queue.size
+        }
+        return result
     }
 
-    fun output(queue: MutableList<Char>, set: HashSet<Char>, str: String, c: Char) {
-        if (str.length < queue.size) {
-            queue.flatMap{}
-        }
-        while (queue.get(0) != c) {
-
-        }
-    }
 }
