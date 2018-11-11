6600a2672b0be1f02d7ab0722a89a107e169f321
nerolink <3053318657@qq.com>
2018-11-11 08:50:51 +0800

    1025

--- src/medium/Solution120.kt
+++ src/medium/Solution120.kt
@@ -5,8 +5,17 @@ import java.util.*
 class Solution120 {
 
     fun minimumTotal(triangle: List<List<Int>>): Int {
-        val temp = LinkedList<Int>()
-
+        var temp = LinkedList<Int>()
+        var result = LinkedList<Int>()
+        result.addAll(triangle[triangle.size - 1])
+        for (i in triangle.size - 2 downTo 0) {
+            temp = result
+            result = LinkedList<Int>()
+            for (j in 0 until triangle[i].size) {
+                result.add(triangle[i][j] + Math.min(temp[j], temp[j + 1]))
+            }
+        }
+        return result[0]
     }
 
 }
